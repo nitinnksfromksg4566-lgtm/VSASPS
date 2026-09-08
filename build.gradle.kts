b46cd5d782573de3aaada196028f7fbd20d8dd0f
nitinnksfromksg4566-lgtm <nitinnksfromksg4566@gmail.com>
2026-09-08 18:12:20 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+android {
+    namespace = "com.vsenterprises.attendance"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.vsenterprises.attendance"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
 }
